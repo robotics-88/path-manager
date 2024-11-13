57965d3ff9f419e436b672af2e6ae2eb4040ce26
Gus Meyer <gus@robotics88.com>
2024-11-13 11:32:24 -0800

    Using path planner as a service

--- include/path_manager/path_manager.h
+++ include/path_manager/path_manager.h
@@ -96,6 +96,7 @@ class PathManager : public rclcpp::Node
         bool isCloseToGoal();
         bool adjustGoal(geometry_msgs::msg::PoseStamped goal);
         void publishGoal(geometry_msgs::msg::PoseStamped goal);
+        bool requestPath(const geometry_msgs::msg::PoseStamped goal);
         void adjustAltitudeVolume(const geometry_msgs::msg::Point &map_position, double &target_altitude);
 };
 
